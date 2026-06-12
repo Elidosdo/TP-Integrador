c0197842a9d54782fd94b3a35b8eb46c5314afa1
Elidosdo <eliasgarciainfante10@gmail.com>
2026-06-12 17:03:42 -0300

    Creamos menu principal

--- main.py
+++ main.py
@@ -9,3 +9,42 @@ with open("paises.csv", "w", newline="", encoding="utf-8") as archivo:
     escritor.writerow(["Alemania", 83149300, 357022, "Europa"])
 
 print("CSV creado")
+
+def cargar_menu():
+    while True:
+        try:
+            print("=== Gestion de paises ===")
+            print("1) Agregar pais")
+            print("2) Actualizar pais")
+            print("3) Buscar pais")
+            print("4) Filtrar paises")
+            print("5) Ordenar paises")
+            print("6) Estadisticas")
+            print("7) Salir")
+            
+            opcion = input("Selecciona una opcion: ")
+            opcion = int(opcion)
+        except ValueError:
+            print("Debe ingresar numeros") 
+        else:
+            if opcion == "":
+                print("No se permiten vacios")
+
+            if opcion == 1:
+                pass
+            elif opcion == 2:
+                pass
+            elif opcion == 3:
+                pass
+            elif opcion == 4:
+                pass
+            elif opcion == 5:
+                pass
+            elif opcion == 6:
+                pass
+            elif opcion == 7:
+                print("Cerrando programa...")
+                break
+            else:
+                print("Ingrese una opcion valida")
+cargar_menu()
